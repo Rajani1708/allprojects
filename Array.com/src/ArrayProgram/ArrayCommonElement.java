8710f04da47f809aa95bb3060b2b721eef38e17e
Rajani <singh.rajani1805@gmail.com>
2023-06-11 12:54:49 +0530

    hiiii

---
+++ Array.com/src/ArrayProgram/ArrayCommonElement.java
@@ -0,0 +1,27 @@
+package ArrayProgram;
+
+public class ArrayCommonElement {
+
+	public static void main(String[]args) {
+		
+		
+			int[] x1 = {10,73,81,92,68,27};
+			int[] y2 = {11,51,10,92,81,73};
+			
+			for(int i=0; i < x1.length; i++) {
+				fir(int j= 0; j < y2.length; j++){
+					
+					if(x1[i] == y2[j]) {
+						
+						System.out.println(x1[i]);
+
+					}
+		
+				}
+			}	
+	}
+
+
+		
+		
+}
